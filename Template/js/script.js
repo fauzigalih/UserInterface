24af551b058498bfcbc8f87e185b1c0b2aecffe4
Fauzi Galih Aji Saputro <64176403+fauzigalih@users.noreply.github.com>
2020-09-04 10:40:34 +0700

    Update script.js

--- Template/js/script.js
+++ Template/js/script.js
@@ -11,5 +11,19 @@ $(document).ready(function(){
         const scr = $(window).scrollTop();
         if(scr > 75) $(".nav").addClass("sticky");
         else $(".nav").removeClass("sticky");
+
+    });
+    
+    $(".bxslider").bxSlider({
+        mode: 'horzontal',
+        moveSlides:1,
+        infiniteLoop:true,
+        minSlides:1,
+        maxSlides:1,
+        stopAutoOnClick: true,
+        // auto: true,
+        autoControls: true,
+        pager: true,
+        speed:1200,
     });
 })
